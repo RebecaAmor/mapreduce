d56710d9c65a37f3c32b1de6d69decc35926065d
Becky <92539914+RebecaAmor@users.noreply.github.com>
2022-12-14 20:44:13 +0100

    modificación 1

--- exercicio1_3/reducer.py
+++ exercicio1_3/reducer.py
@@ -2,34 +2,19 @@
 
 import sys
 
-mayorventa = 0
-oldKey = None
-
-# Loop around the data
-# It will be in the format key\tval
-# Where key is the store name, val is the sale amount
-#
-# All the sales for a particular store will be presented,
-# then the key will change and we'll be dealing with the next store
+highest_sales = {}
 
 for line in sys.stdin:
-    data_mapped = line.strip().split("\t")
-    if len(data_mapped) != 2:
-        # Something has gone wrong. Skip this line.
+    data = line.strip().split("\t")
+    date, time, store, item, cost, payment = data
+
+    if len(data) != 6:
         continue
 
-    thisKey, thisSale = data_mapped
-
-    # Escribe un par key:value ante un cambio na key
-    # Reinicia o total
-    if oldKey and oldKey != thisKey:
-        print(oldKey+"\t"+str(mayorventa))
-        oldKey = thisKey;
-        mayorventa = 0
-
-    oldKey = thisKey
-    mayorventa += float(thisSale)
-
-# Escribe o ultimo par, unha vez rematado o bucle
-if oldKey != None:
-    print(oldKey+"\t"+str(mayorventa))
+    if payment in highest_sales:
+        highest_sales[payment] = max(highest_sales[payment], float(cost))
+    else:
+        highest_sales[payment] = float(cost)
+        
+for payment, sale in highest_sales.items():
+    print(payment+": "+str(sale))
