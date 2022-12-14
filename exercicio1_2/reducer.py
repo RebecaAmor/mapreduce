422742f29f0f51c9429726031d0e6bd82a24c28c
Becky <92539914+RebecaAmor@users.noreply.github.com>
2022-12-14 20:42:46 +0100

    modificación 1

--- exercicio1_2/reducer.py
+++ exercicio1_2/reducer.py
@@ -1,35 +1,20 @@
 #!/usr/bin/python
-
 import sys
 
-salesTotal = 0
-oldKey = None
-
-# Loop around the data
-# It will be in the format key\tval
-# Where key is the store name, val is the sale amount
-#
-# All the sales for a particular store will be presented,
-# then the key will change and we'll be dealing with the next store
+sales_totals = {}
 
 for line in sys.stdin:
-    data_mapped = line.strip().split("\t")
-    if len(data_mapped) != 2:
-        # Something has gone wrong. Skip this line.
+    data = line.strip().split("\t")
+
+    if len(data) != 2:
         continue
 
-    thisKey, thisSale = data_mapped
+    this_key, this_sale = data
 
-    # Escribe un par key:value ante un cambio na key
-    # Reinicia o total
-    if oldKey and oldKey != thisKey:
-        print(oldKey+"\t"+str(salesTotal))
-        oldKey = thisKey;
-        salesTotal = 0
+    if this_key in sales_totals:
+        sales_totals[this_key] += float(this_sale)
+    else:
+        sales_totals[this_key] = float(this_sale)
 
-    oldKey = thisKey
-    salesTotal += float(thisSale)
-
-# Escribe o ultimo par, unha vez rematado o bucle
-if oldKey != None:
-    print(oldKey+"\t"+str(salesTotal))
+for this_key, total in sales_totals.items():
+    print(this_key+": "+str(total))
